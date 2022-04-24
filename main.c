437a4daf8aeb78435751a9253195c85cf364aa0a
David Tan <32520157+skytruong90@users.noreply.github.com>
2022-04-24 01:02:33 -0400

    Add files via upload

---
+++ main.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    printf("\n Welcome to The Security System");
+    int password;
+    int FakeInput;
+    printf("\n\n Please type your password ");
+    scanf("%d", &password);
+
+    if(password <= 12345 && password >= 12345 ){
+        printf("\n The password is correct");
+        printf("\n Welcome and Thank you for cracking the code");
+    }
+    else{
+        printf("\n The password is incorrect");
+        printf("\n You will have 2 more tries!!");
+        printf("\n Try again");
+        scanf("%d", &password);
+
+   if(password <= 12345 && password >= 12345 ){
+        printf("\n The password is correct");
+        printf("\n Welcome and Thank you for cracking the code");
+    }
+    else{
+        printf("\n The password is incorrect");
+        printf("\n You will have 1 more try!!");
+        printf("\n Try again");
+        scanf("%d", &password);
+        
+    if(password <= 12345 && password >= 12345 ){
+        printf("\n The password is correct");
+        printf("\n Welcome and Thank you for cracking the code");
+    }
+    else{
+        printf("\n The password is incorrect");
+    }
+    scanf("\n%d", FakeInput);
+return 0;
+ }
+}
